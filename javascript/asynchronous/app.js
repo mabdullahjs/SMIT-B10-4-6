14f4d5b17627ac16dcccf4d87722af67601c5470
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-10-23 17:24:01 +0500

    try catch and async await

--- javascript/asynchronous/app.js
+++ javascript/asynchronous/app.js
@@ -1,6 +1,108 @@
 
 
-console.log('hello world')
-console.log('hello world2')
-console.log('hello world3')
-console.log('hello world4')
+
+//settimeout
+//setinterval
+//axios
+//fetch
+//promises
+//async await
+
+
+// let data = data req kara ha
+// console.log(data);
+
+
+
+
+
+
+// console.log('hello world');
+// console.log('hello world2');
+// console.log('hello world3');
+// setTimeout(() => {
+//     console.log('Hello world4');
+// }, 0);
+// console.log('hello world5');
+
+
+
+// function rishta (bankBalance){
+//     return new Promise((resolve , reject)=>{
+//         if(bankBalance > 400000){
+//             resolve('rishta pakka hogaya.. istekhara haan ha.')
+//         }else{
+//             reject('abba nahi maan rha.. ammi zehr khalaingi..')
+//         }
+//     })
+// }
+// rishta(10000000).then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getData(){
+//     axios.get('https://jsonplaceholder.typicode.com/users')
+//     .then((res)=>{
+//         console.log(res.data);
+//     }).catch((err)=>{
+//         console.log(err);
+//     })
+// }
+// getData()
+
+
+// async function getData() {
+//     try {
+//         const res = await axios.get('https://jsonplaceholder.typicode.com/user')
+//         console.log(res.data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// }
+
+
+// getData()
+
+
+
+// // aler('hello world')
+// console.log('hello world');
+
+
+
+
+
+// // try {
+// //     aler('hello world')
+// // } catch (error) {
+//     console.log(error);
+// }
+// console.log('hello world');
+
+
+
+
+
+
+
+
+
+
+
+
+
